321ebfaadc5a42252a12df473e87d37bfcb22f7a
Jkim3213 <31422025+Jkim3213@users.noreply.github.com>
2017-11-08 15:02:40 -0500

    files

---
+++ Main.c
@@ -0,0 +1,303 @@
+#include <stdio.h>
+#include "Main.h"
+
+unsigned short currentColor;
+
+
+WOLF wolves[3];
+int wlfCnter = 0;//wolf counter
+int laserPos; 
+LASER laser;
+int laserRow;
+int laserCol;
+
+int main()
+{
+	REG_DISPCTL = MODE3 | BG2_ENABLE;
+	shpAdjRow = ADJPOS(shpRow, shpHeight);
+	shpAdjCol = ADJPOS(shpCol, shpWidth);
+	maxWolves = 3;
+	laserGirth = shpHeight/2;
+	laserRow = shpRow - laserGirth/2;
+   	laserCol = shpCol + shpWidth/2;
+	
+	
+	//int score = 0;
+	//char buffer[41];
+	//sprintf(buffer, "Score: %d", score); //max score count
+	//drawString(150, 5, buffer, YELLOW);
+// Fill screen with bgcolor using DBA
+	int bgcolor = BGCOLOR;
+	REG_DMA3SAD = (u32)&bgcolor; // This is always an address!
+	REG_DMA3DAD = (u32)videoBuffer; // This is always an address!
+	REG_DMA3CNT = (240*160) | DMA_ON | DMA_DESTINATION_INCREMENT | DMA_SOURCE_FIXED;
+	//drawRect(shpRow - shpHeight/2, shpCol - shpWidth/2, shpHeight, shpWidth, BLACK);
+	updateColor(0);
+	updateSheep();
+	volatile unsigned int laserCooldown = 0;
+	
+	unsigned int wolfCooldown = 120;
+	const int wolfRate = 60;//8 wolves a sec
+
+	
+	while(1)
+	{
+	  //LIGHT COLORS
+	  if(KEY_DOWN_NOW(BUTTON_LEFT) && KEY_DOWN_NOW(BUTTON_A))
+	    {
+	      updateColor(1);
+	      updateSheep();    
+	    }
+	  if(KEY_DOWN_NOW(BUTTON_UP) && KEY_DOWN_NOW(BUTTON_A))
+	    {
+	      updateColor(2);
+	      updateSheep(); 
+	    }
+	  if(KEY_DOWN_NOW(BUTTON_RIGHT) && KEY_DOWN_NOW(BUTTON_A))
+	    {
+	      updateColor(3);
+	      updateSheep();
+	    }
+	  //DARK COLORS
+	  if(KEY_DOWN_NOW(BUTTON_LEFT) && KEY_DOWN_NOW(BUTTON_B))
+	    {
+	      updateColor(4);
+	      updateSheep();
+	    }
+	  if(KEY_DOWN_NOW(BUTTON_UP) && KEY_DOWN_NOW(BUTTON_B))
+	    {
+	      updateColor(5);
+	      updateSheep();
+	    }
+	  if(KEY_DOWN_NOW(BUTTON_RIGHT) && KEY_DOWN_NOW(BUTTON_B))
+	    {
+	      updateColor(6);
+	      updateSheep();
+	    }
+	  //RESET
+	  if(KEY_DOWN_NOW(BUTTON_DOWN))
+	    {
+	      	updateColor(0);
+	      	updateSheep();
+	    }
+	  if((KEY_DOWN_NOW(BUTTON_L) || KEY_DOWN_NOW(BUTTON_R)) && laserCooldown == 0)
+	    {
+	    	laserCooldown = lCD;
+	      	shootLaser();
+	      	updateColor(0);
+	      	updateSheep();
+	    }
+
+	    if(wolfCooldown == 0)
+	    {
+	    	createWolf();
+	    	wolfCooldown = wolfRate;
+	    }
+
+	    updateWolves();
+	    updateLaser();
+
+	    if(laserCooldown)
+	    	laserCooldown--;
+
+	    if(wolfCooldown)
+	    	wolfCooldown--;
+
+	  	waitForVblank();
+	  
+	}
+
+}
+
+void shootLaser()
+{
+  
+  
+/*
+  //160x240 dim of screen
+  //drawRect(laserRow, laserCol, laserGirth, 240 - laserCol, currentColor);
+  const int growth = (240 - laserCol)/5;//divide by multiples of 5: 240-laserCol is 50.
+  for(int i = laserCol; i < 240; i = i + growth){
+    drawRect(laserRow, i, laserGirth, growth, currentColor);
+    //waitForVblank();
+  }
+  const int decay = (240 - laserCol)/10;
+  for(int i = laserCol; i < 240; i = i + decay){
+    drawRect(laserRow, i, laserGirth, decay, BGCOLOR);
+    //waitForVblank();
+  }
+*/
+  LASER temp = {1, currentColor, laserRow, laserCol, laserSpeed};
+  laser = temp;
+}
+
+void updateLaser()
+{
+	//TODOx/
+	static volatile int laserTime = 120;
+	if(laser.exists && (laserTime == 0))
+	{
+		laser.col = 0;
+		laser.exists = 0;
+		drawRect(laser.row, laserCol, laserGirth, 190, BGCOLOR);
+	}	
+	if(laser.exists)
+	{
+		//drawRect(laser.row, laser.col, laserGirth, laser.speed, BGCOLOR);
+		if(laserTime == 0)
+			laserTime = 180;
+		laser.col = laser.col + laser.speed;
+		if(laser.col + laser.speed < 240)
+			drawRect(laser.row, laser.col, laserGirth, laser.speed, laser.color);
+		
+	}
+	if(laserTime > 0)
+		laserTime--;
+
+}
+
+void updateSheep()
+{
+  	drawRect(shpAdjRow - 1, shpAdjCol - 1, shpHeight + 2, shpWidth + 2, currentColor - currentColor/2);	
+  	drawRect(shpAdjRow, shpAdjCol, shpHeight, shpWidth, currentColor);
+}
+
+void updateColor(int x)
+{
+  switch(x){
+
+  case 0:
+    currentColor = BLACK;
+    break;
+  case 1:
+    currentColor = currentColor & ~RED;
+    currentColor = currentColor | RED;
+    break;
+  case 2:
+    currentColor = currentColor & ~GREEN;
+    currentColor = currentColor | GREEN;
+    break;
+  case 3:
+    currentColor = currentColor & ~BLUE;
+    currentColor = currentColor | BLUE;
+    break;
+  case 4:
+    currentColor = currentColor & ~RED;
+    currentColor = currentColor | DRED;
+    break;
+  case 5:
+    currentColor = currentColor & ~GREEN;
+    currentColor = currentColor | DGREEN;
+    break;
+  case 6:
+    currentColor = currentColor & ~BLUE;
+    currentColor = currentColor | DBLUE;
+    break;
+  
+    
+  }
+}
+
+void setPixel(int row, int col, unsigned short color)
+{
+	videoBuffer[OFFSET(row, col, 240)] = color;
+}
+
+void drawRect(int row, int col, int height, int width, unsigned short color)
+{
+	for(int r=0; r<height; r++)
+	{
+		REG_DMA3SAD = (u32)&color; // This is always an address!
+		REG_DMA3DAD = (u32)&videoBuffer[OFFSET(row+r, col, 240)]; // This is always an address!
+		//REG_DMA3DAD = (u32)(videoBuffer + OFFSET(row+r, col, 240) ); // This is always an address!
+		REG_DMA3CNT = (width) | DMA_ON | DMA_DESTINATION_INCREMENT | DMA_SOURCE_FIXED;
+	}
+}
+
+/*
+void spawnWolf(int speed, short color, short health, short spawnType)
+{
+	//WOLF wolf = {speed, color, health, spawnType, wlfRow, wlfCol};
+	//unsigned int wRectRow = ADJPOS(wlfRow, dwlfHeight + health*gFactor);
+	//unsigned int wRectCol = ADJPOS(wlfCol, dwlfWidth + health*gFactor); I want it to spawn at the edge of the screen
+
+	
+
+}
+*/
+void createWolf()
+{
+	unsigned int wRectRow = ADJPOS(wlfRow, dwlfHeight);
+	WOLF wolf = {1, RED, 5, 0, wRectRow, wlfCol, 1};
+	if(wlfCnter < maxWolves){
+		wolves[wlfCnter] = wolf;
+		wlfCnter++;
+	}
+
+}
+
+void updateWolves()
+{
+	for(int i = 0; i < wlfCnter; i++)
+	{
+
+		if(wolves[i].alive)
+		{		
+			drawRect(wolves[i].row, wolves[i].col, dwlfHeight, dwlfWidth, BGCOLOR);
+			wolves[i].col = wolves[i].col - wolves[i].speed;
+			if(wolves[i].col <= shpDeathCol)
+			{
+				wolves[i].alive = 0;
+				wlfCnter--;
+			
+			}
+			else
+			{
+				drawRect(wolves[i].row, wolves[i].col, dwlfHeight, dwlfWidth, wolves[i].color);		
+			}
+		}
+
+	}
+
+}
+
+
+void delay(int n)
+{
+	volatile int x = 0;
+	for(int i=0; i<n*5000; i++)
+	{
+		x = x + 1;
+	}
+}
+
+void waitForVblank()
+{
+	while(SCANLINECOUNTER > 160);
+	while(SCANLINECOUNTER < 160);
+}
+
+void drawChar(int row, int col, char ch, unsigned short color)
+{
+	for(int r = 0; r< 8; r++)
+	{
+		for(int c=0; c< 6; c++)
+		{
+			if( fontdata_6x8[OFFSET(r, c, 6) + ch*48] == 1)
+			{
+				setPixel(r+row, c+col, color);
+			}
+		}
+
+	}
+}
+
+void drawString(int row, int col, char str[], unsigned short color)
+{
+	while(*str)
+	{
+		drawChar(row, col, *str++, color);
+		col += 6;
+	}
+}
+
---
+++ Main.h
@@ -0,0 +1,168 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+typedef unsigned int u32;
+
+#define REG_DISPCTL *(unsigned short *)0x4000000
+#define MODE3 3
+
+#define SCANLINECOUNTER *(volatile unsigned short *) 0x4000006
+
+#define SCREENHEIGHT 149
+
+#define BG2_ENABLE (1<<10)
+#define COLOR(r, g, b) ((r) | (g)<<5 | (b)<<10)
+#define RED COLOR(31,0,0)
+#define GREEN COLOR(0,31,0)
+#define BLUE COLOR(0,0,31)
+#define DRED COLOR(18, 0, 0)
+#define DGREEN COLOR(0, 18, 0)
+#define DBLUE COLOR(0, 0, 18)
+#define YELLOW COLOR(31, 31,0)
+#define WHITE COLOR(31, 31, 31)
+#define BGCOLOR COLOR(29, 29, 29)
+#define OFFSET(r, c, rowlen) ((r)*(rowlen)+(c))
+#define BLACK 0
+
+
+extern const unsigned char fontdata_6x8[12288];
+
+// Buttons
+
+#define BUTTON_A		(1<<0)
+#define BUTTON_B		(1<<1)
+#define BUTTON_SELECT	        (1<<2)
+#define BUTTON_START	        (1<<3)
+#define BUTTON_RIGHT	        (1<<4)
+#define BUTTON_LEFT		(1<<5)
+#define BUTTON_UP		(1<<6)
+#define BUTTON_DOWN		(1<<7)
+#define BUTTON_R		(1<<8)
+#define BUTTON_L		(1<<9)
+
+#define KEY_DOWN_NOW(key)  (~(BUTTONS) & key)
+
+#define BUTTONS *(volatile unsigned int *)0x4000130
+
+/* DMA */
+
+#define REG_DMA0SAD         *(volatile u32*)0x40000B0 		// source address
+#define REG_DMA0DAD         *(volatile u32*)0x40000B4       // destination address
+#define REG_DMA0CNT         *(volatile u32*)0x40000B8       // control register
+
+// DMA channel 1 register definitions
+#define REG_DMA1SAD         *(volatile u32*)0x40000BC 		// source address
+#define REG_DMA1DAD         *(volatile u32*)0x40000C0       // destination address
+#define REG_DMA1CNT         *(volatile u32*)0x40000C4       // control register
+
+// DMA channel 2 register definitions
+#define REG_DMA2SAD         *(volatile u32*)0x40000C8 		// source address
+#define REG_DMA2DAD         *(volatile u32*)0x40000CC       // destination address
+#define REG_DMA2CNT         *(volatile u32*)0x40000D0       // control register
+
+// DMA channel 3 register definitions
+#define REG_DMA3SAD         *(volatile u32*)0x40000D4 		// source address
+#define REG_DMA3DAD         *(volatile u32*)0x40000D8       // destination address
+#define REG_DMA3CNT         *(volatile u32*)0x40000DC       // control register
+
+
+typedef struct
+{
+	const volatile void *src;
+	volatile void *dst;
+	unsigned int cnt;
+} DMA_CONTROLLER;
+
+#define DMA ((volatile DMA_CONTROLLER *) 0x040000B0)
+
+// Defines
+#define DMA_CHANNEL_0 0
+#define DMA_CHANNEL_1 1
+#define DMA_CHANNEL_2 2
+#define DMA_CHANNEL_3 3
+
+#define DMA_DESTINATION_INCREMENT (0 << 21)
+#define DMA_DESTINATION_DECREMENT (1 << 21)
+#define DMA_DESTINATION_FIXED (2 << 21)
+
+
+#define DMA_SOURCE_INCREMENT (0 << 23)
+#define DMA_SOURCE_DECREMENT (1 << 23)
+#define DMA_SOURCE_FIXED (2 << 23)
+
+#define DMA_REPEAT (1 << 25)
+
+#define DMA_16 (0 << 26)
+#define DMA_32 (1 << 26)
+
+#define DMA_NOW (0 << 28)
+#define DMA_AT_VBLANK (1 << 28)
+#define DMA_AT_HBLANK (2 << 28)
+#define DMA_AT_REFRESH (3 << 28)
+
+#define DMA_IRQ (1 << 30)
+#define DMA_ON (1 << 31)
+
+#define ADJPOS(pos, length) ((pos) - (length)/2)
+
+const int shpRow = 88;//describes center of sheep
+const int shpCol = 40;//describes center of sheep
+const int shpWidth = 20;
+const int shpHeight = 15;
+const unsigned int shpDeathCol = 50;//shpCol + shpWidth/2
+const int wlfRow = 88;
+const int wlfCol = 239;
+const int dwlfWidth = 20;
+const int dwlfHeight = 15;
+const int gFactor = 5; //growthfactor for wolves
+const unsigned int lCD = 30;//the constant value that laserCoolDown refreshes to
+const unsigned int laserSpeed = 19;
+int maxWolves;
+
+int shpAdjRow;
+int shpAdjCol;
+int laserGirth;
+
+
+unsigned short *videoBuffer = (unsigned short *)0x6000000;
+extern unsigned short currentColor;//store color of sheep
+
+// Prototypes
+void shootLaser();//Shoots current color and resets to black sheep
+void updateLaser();
+void updateSheep();//draws sheep
+void updateColor(int color);//updates color value
+void setPixel(int row, int col, unsigned short color);
+void drawRect(int row, int col, int height, int width, unsigned short color);
+void spawnWolf(int speed, short color, short health, short spawnType);
+void createWolf();//actually creates the wolf
+void updateWolves();
+void delay(int n);
+void waitForVblank();
+void drawChar(int row, int col, char ch, unsigned short color);
+void drawString(int row, int col, char str[], unsigned short color);
+
+typedef struct WOLF
+{
+	unsigned int speed;
+	unsigned short color;
+	unsigned short health;//directly related to size
+	unsigned short spawnType;//0 - normal. 1 - rushed start. 2 - airborne
+	unsigned int row;
+	unsigned int col;
+	unsigned short alive;//1 or 0
+
+} WOLF;
+
+typedef struct LASER
+{
+	unsigned short exists;
+	unsigned short color;
+	unsigned int row;
+	unsigned int col;
+	unsigned int speed;
+} LASER;
+
+
+
+#endif
